13ba17474662edaebb28d0d33d2a24f8be44bf20
Madcyclist17 <114353046+Madcyclist17@users.noreply.github.com>
2024-04-19 11:39:29 +0530

    new bank.cpp

--- bank.cpp
+++ bank.cpp
@@ -5,18 +5,27 @@ class Bank
 private:
 int members[3];
 char name[35];
-char acc[3];
+int acc;
 int total=0,amount,bala,c,choice;
 char type[1];
 public:
+
+
+
 void read()
 {
-cout<<"enter the name "<<endl;
+cout<<"enter the name :"<<endl;
 cin>>name;
-cout<<"enter the account number "<<endl;
+cout<<"enter the account number: "<<endl;
 cin>>acc;
-cout<<"enter the type "<<endl;
+cout<<"enter the type(Saving 'S' & Current 'C') "<<endl;
 cin>>type;
+
+        cout<<"\n ----------------------";
+        cout<<"\n Accout No. : "<<acc;
+        cout<<"\n Name : "<<name;
+        cout<<"\n Account Type : "<<type; 
+	 cout<<"\n ----------------------"<<endl;
 }
 void deposite()
 {
@@ -45,12 +54,28 @@ void balance()
 bala=total;
 cout<<"the balance amount is "<<bala<<endl;
 }
-void display()
+void details()
 {
-do{
-cout<<"enter the account number";
+        cout<<"\n ----------------------";
+        cout<<"\n Accout No. : "<<acc;
+        cout<<"\n Name : "<<name;
+        cout<<"\n Account Type : "<<type;
+        cout<<"\n Balance : "<<bala;  
+	 cout<<"\n ----------------------"<<endl;
+}
+/*void checkacc(int inputacc)
+{
+if(inputacc==acc)
+display();
+else
+cout<<"enter the valid acc no:"<<endl;
+}*/
 
-cout<<"enter the choice"<<endl<<"1.deposite"<<endl<<"2.withdraw"<<endl<<"3.balance"<<endl;
+void display(int inputacc)
+{
+if(inputacc==acc){
+do{
+cout<<"enter the choice"<<endl<<"1.deposite"<<endl<<"2.withdraw"<<endl<<"3.balance"<<endl<<"4.details"<<endl;
 cin>>choice;
 switch(choice)
 {
@@ -60,31 +85,44 @@ case 2:withdraw();
 	break;
 case 3:balance();
 	break;
+case 4:details();
 default:;
 }
-/*else 
-cout<<"enter the correct account no"<<endl;
-*/
+cout<<"to continue press 1"<<endl;
+cin>>c;
 }
 while(c==1);
 }
-
-void details()
-{
-
-cout<<name<<endl;
-cout<<acc<<endl;
-cout<<type<<endl;
-cout>>bala;
-
+else 
+cout<<"enter the correct account no"<<endl;
 }
+
+
 };
 int main()
 {
-int i;
-Bank a[3];
-for(i=0;i<=3;i++)
-a.read();
-
+int i,acc,n;
+cout<<"enter no.of account holders"<<endl;
+cin>>n;
+Bank a[n];
+for(i=0;i<n;i++)
+{
+a[i].read();
 }
 
+cout<<"enter the account number :";
+cin>>acc;
+for(i=0;i<n;i++)
+a[i].display(acc);
+cout<<"enter the account number :";
+cin>>acc;
+for(i=0;i<n;i++)
+a[i].display(acc);
+cout<<"enter the account number :";
+cin>>acc;
+for(i=0;i<n;i++)
+a[i].display(acc);
+
+for(i=0;i<n;i++)
+a[i].details();
+}
